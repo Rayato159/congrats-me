b7246be04067a6beddd57356ef2920fef989eee6
Rayato159 <rayato159@gmail.com>
2022-04-05 00:45:00 +0700

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.b6658688.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcongrats_me=self.webpackChunkcongrats_me||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.b6658688.chunk.js.map
